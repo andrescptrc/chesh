7e34a0bc09ad8165087cd1ebb3dee05efb07abc2
Brayan Carreño <andrebrayan74@gmail.com>
2023-12-14 16:48:53 -0500

    chore: add the loop base

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 void loop(void);
 char *read_line(void);
 char **split_line(char *line);
 int execute(char **args);
+
+void loop(void) {
+
+  int status = 1;
+
+  do {
+    printf("🍺 > ");
+  } while (status);
+
+}
+
 
 int main(void) {
   return EXIT_SUCCESS;
